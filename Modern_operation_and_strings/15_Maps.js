f904d83b664ed13b95f038615cd9532d2e6ad0a5
Chandra Sekhar Reddy Ramireddy <ramireddychandu77@gmail.com>
2022-08-01 21:34:12 +0530

    Added New Files -- Maps and Strings Concepts --

---
+++ Modern_operation_and_strings/15_Maps.js
@@ -0,0 +1,9 @@
+const rest = new Map();
+rest.set("Name", "Chandu");
+rest.set(1, "Peddanapadu, Yerraguntla");
+rest
+  .set("phone", 9550)
+  .set("add", "ped")
+  .set("Categories", ["List1", "List2", "List3"]);
+console.log(rest);
+console.log(typeof rest);
