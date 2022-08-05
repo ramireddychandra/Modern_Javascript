0b3bdc012b948877cf6eefc4bf7452616ea899fc
Chandra Sekhar Reddy Ramireddy <ramireddychandu77@gmail.com>
2022-08-05 20:12:18 +0530

    Included Array operations and methods

---
+++ Arrays/01_Simple_array_methods.js
@@ -0,0 +1,32 @@
+// Simple Array Methods
+let arr = ["a", "b", "c", "d", "e"];
+
+// SLICE
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log([...arr]);
+
+// SPLICE
+// console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+// REVERSE
+arr = ["a", "b", "c", "d", "e"];
+const arr2 = ["j", "i", "h", "g", "f"];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// CONCAT
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+// JOIN
+console.log(letters.join(" - "));
---
+++ Arrays/03_for_each_loop.js
@@ -0,0 +1,40 @@
+///////////////////////////////////////
+// Looping Arrays: forEach
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('---- FOREACH ----');
+//  First argument is iterative element, i is index, arr is original array...
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
+
+const arr = ['apple', 'bappaya', 'orange'];
+arr.forEach(function (elements, i, array) {
+  console.log(elements, i, array);
+});
+
+onst arr = ['apple', 'bappaya', 'orange'];
+arr.forEach(function (
+  elements /* iterative element*/,
+  i /*index*/,
+  array /* always original array*/
+) {
+  console.log(elements, i, array);
+});
+// 0: function(200)
+// 1: function(450)
+// 2: function(400)
+// ...
