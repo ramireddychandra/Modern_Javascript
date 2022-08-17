fcbfd6296d3668dddac74baf3fc123c1e07cb37d
Chandra Sekhar Reddy Ramireddy <ramireddychandu77@gmail.com>
2022-08-17 20:56:23 +0530

    Operations on Array.

---
+++ Arrays/20_flat_and_flatmap_functions.js
@@ -0,0 +1,20 @@
+///////////////////////////////////////
+// flat and flatMap
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+// flat
+const overalBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance);
+
+// flatMap
+const overalBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance2);
